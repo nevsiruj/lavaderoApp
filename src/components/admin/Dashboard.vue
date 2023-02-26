6e446269434060f71ae5f8d707ad468812ca7aac
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-02-25 23:06:44 -0300

    1 file modified, 3 added

---
+++ src/components/admin/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container-fluid">
+    <Navbar />
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Lavados por día</h2>
+        <LavadosChart />
+      </div>
+      <div class="col-md-6">
+        <h2>Ingresos y egresos</h2>
+        <IngresosEgresosChart />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar.vue';
+import LavadosChart from './LavadosChart.vue';
+// import IngresosEgresosChart from './IngresosEgresosChart.vue';
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Navbar,
+    LavadosChart,
+    // IngresosEgresosChart,
+  },
+};
+</script>
---
+++ src/components/admin/LavadosChart.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <canvas ref="chartRef"></canvas>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import Chart from 'chart.js/auto';
+
+export default {
+  name: 'LavadosChart',
+  setup() {
+    const chartRef = ref(null);
+
+    onMounted(() => {
+      const ctx = chartRef.value.getContext('2d');
+
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'],
+          datasets: [
+            {
+              label: 'Cantidad de lavados',
+              data: [12, 19, 3, 5, 2],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    });
+
+    return {
+      chartRef,
+    };
+  },
+};
+</script>
+
+<style scoped>
+canvas {
+  width: 100%;
+  height: 400px;
+}
+</style>
