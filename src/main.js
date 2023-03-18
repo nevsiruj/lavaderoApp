1949b10bff3e0479b855342d2fdf5882e78f7ff9
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-03-18 01:44:34 -0300

    1 file modified

--- src/main.js
+++ src/main.js
@@ -2,6 +2,4 @@ const { createApp } = require('vue');
 import App from './App.vue';
 import router from './router'; // <---
 
-console.log(process.env.VUE_APP_API_URL);
-
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(process.env).mount('#app');
