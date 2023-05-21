344529ac2775ea80122490a6cc4ff19c72c91a92
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-21 19:04:13 -0300

    1 file modified

--- src/components/caja/Caja.vue
+++ src/components/caja/Caja.vue
@@ -1,89 +1,73 @@
 <template>
-  <div class="row">
-    <h3 class="mt-3">Bienvenido</h3>
-  </div>
+  <div class="flex flex-col items-center">
+  <h3 class="mt-3 text-2xl font-bold">Bienvenido</h3>
+</div>
 
-  <div class="container">
-    <div class="card">
-      <div class="card-header">
-        <h5>
-          {{ new Date().toLocaleDateString() }}
-        </h5>
+<div class="container mx-auto">
+  <div class="card shadow-lg">
+    <div class="card-header bg-blue-500 py-2">
+      <h5 class="text-white text-lg font-semibold">
+        {{ new Date().toLocaleDateString() }}
+      </h5>
+    </div>
+    <div class="card-body p-4">
+      <div v-if="cajaAbierta.isOpen">
+        <p class="card-text">
+          <strong>Responsable:</strong> {{ cajaAbierta.responsable }}
+        </p>
+        <label class="card-text">
+          Dinero en la caja: ${{ cajaAbierta.montoInicial ?? '0.00' }}
+        </label>
+        <router-link class="btn btn-sm btn-danger mt-2" to="/cerrarcaja">
+          <i class="fa fa-times-circle mr-1"></i> Cerrar caja
+        </router-link>
+        <br />
+        <br />
+        <label class="card-text">
+          <strong>Cantidad Lavados:</strong> {{ cajaAbierta.cantidadLavados ?? 0 }}
+        </label>
+        <router-link class="btn btn-sm btn-success mt-2" to="/formlavado">
+          <i class="fas fa-plus-circle mr-1"></i> Agregar lavado
+        </router-link>
+        <router-link class="btn btn-sm btn-outline-success mt-2" to="/listlavado">
+          <i class="fas fa-list-ul mr-1"></i> Listado de lavados
+        </router-link>
+        <br />
+        <br />
+        <label class="card-text">
+          <strong>Cantidad Ingresos:</strong> ${{ cajaAbierta.totalImporteIngresos ?? '0.00' }}
+        </label>
+        <router-link class="btn btn-sm btn-success mt-2" to="/formingreso">
+          <i class="fas fa-plus-circle mr-1"></i> Agregar ingreso
+        </router-link>
+        <router-link class="btn btn-sm btn-outline-success mt-2" to="/listingreso">
+          <i class="fas fa-list-ul mr-1"></i> Listado de ingresos
+        </router-link>
+        <br />
+        <br />
+        <label class="card-text">
+          Cantidad Egresos: ${{ cajaAbierta.totalImporteEgresos ?? '0.00' }}
+        </label>
+        <router-link class="btn btn-sm btn-success mt-2" to="/formegreso">
+          <i class="fas fa-plus-circle mr-1"></i> Agregar egreso
+        </router-link>
+        <router-link class="btn btn-sm btn-outline-success mt-2" to="/listegreso">
+          <i class="fas fa-list-ul mr-1"></i> Listado de egresos
+        </router-link>
+        <br />
+        <br />
+        <label class="card-text">
+          Efectivo en caja: ${{ cajaAbierta.efectivoEnCaja ?? '0.00' }}
+        </label>
       </div>
-      <div class="card-body">
-        <div v-if="cajaAbierta.isOpen">
-          <p class="card-text">
-            <strong>Responsable:</strong> {{ cajaAbierta.responsable }}
-          </p>
-          <label class="card-text"
-            >Dinero en la caja: ${{ cajaAbierta.montoInicial ?? '0.00' }}</label
-          >
-          <!-- <button
-            class="btn btn-sm btn-danger ms-2"
-            @click="cerrarCaja(cajaAbierta)"
-          >
-            <i class="fa fa-times-circle" style="font-size: 1rem"></i>
-          </button> -->
-          <router-link class="btn btn-sm btn-danger ms-2" to="/cerrarcaja">
-            <i class="fa fa-times-circle" style="font-size: 1rem"></i>
-          </router-link>
-          <br />
-          <br />
-          <label class="card-text">
-            <strong>Cantidad Lavados:</strong>
-            {{ cajaAbierta.cantidadLavados ?? 0 }}
-          </label>
-          <router-link class="btn btn-sm btn-success ms-2" to="/formlavado"
-            ><i class="fas fa-plus-circle" style="font-size: 1rem"></i>
-          </router-link>
-          <router-link
-            class="btn btn-sm btn-outline-success ms-2"
-            to="/listlavado"
-          >
-            <i class="fas fa-list-ul" style="font-size: 1rem"></i>
-          </router-link>
-          <br />
-          <br />
-          <label class="card-text">
-            <strong>Cantidad Ingresos: </strong>${{
-              cajaAbierta.totalImporteIngresos ?? '0.00'
-            }}
-          </label>
-          <router-link class="btn btn-sm btn-success ms-2" to="/formingreso"
-            ><i class="fas fa-plus-circle" style="font-size: 1rem"></i>
-          </router-link>
-          <router-link
-            class="btn btn-sm btn-outline-success ms-2"
-            to="/listingreso"
-          >
-            <i class="fas fa-list-ul" style="font-size: 1rem"></i>
-          </router-link>
-          <br />
-          <br />
-          <label class="card-text">
-            Cantidad Egresos: ${{ cajaAbierta.totalImporteEgresos ?? '0.00' }}
-          </label>
-          <router-link class="btn btn-sm btn-success ms-2" to="/formegreso"
-            ><i class="fas fa-plus-circle" style="font-size: 1rem"></i>
-          </router-link>
-          <router-link
-            class="btn btn-sm btn-outline-success ms-2"
-            to="/listegreso"
-          >
-            <i class="fas fa-list-ul" style="font-size: 1rem"></i>
-          </router-link>
-          <br />
-          <br />
-          <label class="card-text">
-            Efectivo en caja: ${{ cajaAbierta.efectivoEnCaja ?? '0.00' }}
-          </label>
-        </div>
-        <div class="row" v-if="!cajaAbierta.isOpen">
-          <span class="text-danger"> No hay cajas abiertas </span>
-          <router-link to="/abrircaja">Abrir caja</router-link>
-        </div>
+      <div class="flex flex-col items-center mt-4" v-if="!cajaAbierta.isOpen">
+        <span class="text-red-500 font-bold">No hay cajas abiertas</span>
+        <router-link class="text-blue-500 mt-2" to="/abrircaja">Abrir caja</router-link>
       </div>
     </div>
+  </div>
+</div>
+
   </div>
 
   <!-- <button class="btn btn-outline-dark mt-2" to="/abrircaja">Abrir caja</button> -->
