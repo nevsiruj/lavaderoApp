4b5fee53a976c7cd84b31ebf559a6eb924bd6008
DaronArg <108632377+DaronArg@users.noreply.github.com>
2023-06-10 11:50:52 -0300

    Egresos admin

---
+++ src/components/admin/Egresos.vue
@@ -0,0 +1,222 @@
+<template>
+  <div>
+    <router-link
+      class="btn btn-sm btn-success mt-2 mr-1"
+      :to="{ path: '/formegreso', query: { isAdmin: true } }"
+    >
+      <i class="fas fa-plus-circle mr-1"></i> Agregar Egreso
+    </router-link>
+    <button class="btn btn-sm btn-primary mt-2" @click="fetchEgresos">
+      <i class="fas fa-sync-alt"></i> Actualizar
+    </button>
+    <div
+      v-if="showMessage"
+      class="bg-green-100 text-green-800 px-4 py-2 rounded-md mt-2"
+    >
+      <i class="fas fa-check-circle mr-1"></i> Actualizados
+    </div>
+
+    <div class="bg-white rounded-lg p-4 shadow-md mx-auto max-w-sm mb-0">
+      <div class="flex items-center mb-2">
+        <i class="fas fa-filter text-gray-600 mr-2"></i>
+        <label class="text-gray-600">Filtrar por fecha:</label>
+      </div>
+      <div class="flex space-x-2">
+        <input
+          type="date"
+          class="border-gray-300 rounded-md p-1 flex-grow"
+          v-model="startDate"
+          @change="filterEgresos"
+        />
+        <span class="text-gray-600">-</span>
+        <input
+          type="date"
+          class="border-gray-300 rounded-md p-1 flex-grow"
+          v-model="endDate"
+          @change="filterEgresos"
+        />
+      </div>
+      <!-- Visor de cantidad de egresos mostrados -->
+      <div class="mt-4 flex justify-between items-center">
+        <div class="flex items-center text-gray-600">
+          <i class="fas fa-clipboard-list mr-1"></i>
+          <span>Egresos: {{ filteredEgresos.length }}</span>
+        </div>
+        <div class="flex items-center text-gray-600">
+          <i class="fas fa-dollar-sign mr-1"></i>
+          <span>Gastado: ${{ calculateTotalImporte() }}</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-white rounded-lg p-4 shadow-md mx-auto mt-1">
+      <table class="min-w-full divide-y divide-gray-200 mt-4">
+        <!-- Table headers -->
+        <thead class="bg-gray-50">
+          <tr>
+            <th
+              class="px-2 py-1 text-xs text-gray-500 uppercase tracking-wider"
+            >
+              Fecha
+            </th>
+            <th
+              class="px-2 py-1 text-xs text-gray-500 uppercase tracking-wider"
+            >
+              Descripción
+            </th>
+            <th
+              class="px-2 py-1 text-xs text-gray-500 uppercase tracking-wider"
+            >
+              Importe
+            </th>
+            <th
+              class="px-2 py-1 text-xs text-gray-500 uppercase tracking-wider"
+            ></th>
+          </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+          <tr v-for="egreso in filteredEgresos" :key="egreso.id">
+            <td class="px-2 py-1 whitespace-nowrap">
+              {{ formatDate(egreso.fechaRegistro) }}
+            </td>
+            <td class="px-2 py-1 whitespace-nowrap">
+              {{ egreso.descripcion }}
+            </td>
+            <td class="px-2 py-1 whitespace-nowrap">${{ egreso.importe }}</td>
+            <td class="px-2 py-1 whitespace-nowrap">
+              <div class="flex space-x-2">
+                <button
+                  class="text-blue-600 hover:text-blue-800 focus:outline-none"
+                  @click="editEgreso(egreso)"
+                >
+                  <i class="fas fa-edit"></i>
+                </button>
+                <button
+                  class="text-red-600 hover:text-red-800 focus:outline-none"
+                  @click="deleteEgreso(egreso.id)"
+                >
+                  <i class="fas fa-trash-alt"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import egresoService from '../../composables/api/egresoService.js';
+import { ref, onMounted, computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+export default {
+  name: 'egresos',
+  setup() {
+    const egresos = ref([]);
+    const startDate = ref('');
+    const endDate = ref('');
+    const showMessage = ref(false);
+    const router = useRouter();
+
+    const fetchEgresos = async () => {
+      try {
+        const response = await egresoService.getEgresos();
+        egresos.value = await response.sort((a, b) => {
+          const fechaA = new Date(a.fecha);
+          const fechaB = new Date(b.fecha);
+
+          showMessage.value = true;
+          setTimeout(() => {
+            showMessage.value = false;
+          }, 2000);
+
+          return fechaB - fechaA;
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const formatDate = (date) => {
+      const formattedDate = new Date(date).toLocaleString('es', {
+        day: '2-digit',
+        month: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      return formattedDate;
+    };
+
+    const filterEgresos = async () => {
+      const filtered = egresos.value.filter((egreso) => {
+        const egresoDate = new Date(egreso.fechaRegistro);
+        const start = startDate.value ? new Date(startDate.value) : null;
+        const end = endDate.value ? new Date(endDate.value) : null;
+        return (!start || egresoDate >= start) && (!end || egresoDate <= end);
+      });
+    };
+
+    const filteredEgresos = computed(() => {
+      if (startDate.value || endDate.value) {
+        return egresos.value.filter((egreso) => {
+          const egresoDate = new Date(egreso.fechaRegistro);
+          const start = startDate.value ? new Date(startDate.value) : null;
+          const end = endDate.value ? new Date(endDate.value) : null;
+          if (end) {
+            end.setDate(end.getDate() + 1);
+          }
+          return (!start || egresoDate >= start) && (!end || egresoDate <= end);
+        });
+      } else {
+        return egresos.value;
+      }
+    });
+
+    // Calcula el total de importe de los egresos mostrados
+    const calculateTotalImporte = () => {
+      return filteredEgresos.value.reduce((total, egreso) => {
+        return total + egreso.importe;
+      }, 0);
+    };
+
+    const editEgreso = (egreso) => {
+      router.push({
+        path: '/formegreso',
+        query: { isAdmin: true, id: egreso.id },
+      });
+    };
+
+    const deleteEgreso = async (egresoId) => {
+      try {
+        await egresoService.deleteEgreso(egresoId);
+        fetchEgresos();
+        // Realiza alguna lógica adicional si es necesario
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    onMounted(async () => {
+      await fetchEgresos();
+      await filterEgresos();
+    });
+
+    return {
+      egresos,
+      startDate,
+      endDate,
+      formatDate,
+      filterEgresos,
+      filteredEgresos,
+      calculateTotalImporte,
+      showMessage,
+      fetchEgresos,
+      deleteEgreso,
+      editEgreso,
+      router,
+    };
+  },
+};
+</script>
