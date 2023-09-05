b0a0ad287b3c44a637e1c67ab8ea5fa62894201a
D02-VM01 <ngonzalosuarez@gmail.com>
2023-09-05 16:08:10 -0300

    ignore

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://lavadero.vylaris.com.ar/api';
-export const API_URL = 'https://localhost:44312';
+export const API_URL = 'https://lavadero.vylaris.com.ar/api';
+// export const API_URL = 'https://localhost:44312';
