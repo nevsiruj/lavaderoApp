0f45fae51215d61c536e4e2b8891628d1c4c20f1
nevsiruj <nevsiruj@nevsiruj>
2024-05-24 14:39:45 -0300

    Testapi

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 //export const API_URL = 'https://lavadero.vylaris.com.ar/api';
 // export const API_URL = 'https://tstprj.somee.com';
 //export const API_URL = 'https://localhost:44312';
-export const API_URL = 'https://tst.blegur.vylaris.online/api';
+//export const API_URL = 'https://tst.blegur.vylaris.online/api';
+
+export const API_URL = 'https://app.gestoflex.com/api';
