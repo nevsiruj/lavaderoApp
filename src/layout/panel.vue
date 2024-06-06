0961552329e55517508b67643a2f7610d50233ea
nevsiruj <nevsiruj@nevsiruj>
2024-06-06 18:06:24 -0300

    LogoDashBoard

--- src/layout/panel.vue
+++ src/layout/panel.vue
@@ -11,7 +11,7 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
       <a href="#" @click.prevent="navigateTo('/caja')" class="flex items-center ps-2.5 mb-5">
-         <!-- <img src="../../dist/img/gestoflexdash.png" class="h-6 me-3 sm:h-7" alt="GestoFlex Logo" /> -->
+         <img src="../Img/gestoflexdash.png" class="h-6 me-3 sm:h-7" alt="GestoFlex Logo" />
       </a>
       <ul class="space-y-2 font-medium">
          <li>
